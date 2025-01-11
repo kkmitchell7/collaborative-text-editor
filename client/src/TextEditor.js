bcebd0a390e8ad77bdfaea4e9df7c49a220852a0
kkmitchell7 <kailen53.mitchell@gmail.com>
2025-01-10 21:41:44 -0800

    Added toolbar options

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -3,6 +3,18 @@ import Quill from 'quill'
 import './styles.css'
 import "quill/dist/quill.snow.css"
 
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ]
+
 export default function TextEditor() {
     //Prevents duplication of quill element upon rerender
     const wrapperRef = useCallback((wrapper)=>{ //wrapper is div object, useCallback invoked with the DOM element attached/detached from component
@@ -10,7 +22,7 @@ export default function TextEditor() {
         wrapper.innerHTML = '' //reset the div
         const editor = document.createElement('div') //create new div
         wrapper.append(editor) //append new div to wrapper
-        new Quill(editor,{ theme: 'snow'}); //create Quill instance in new div
+        new Quill(editor,{ theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}}); //create Quill instance in new div
 
     },[]) //stable function across renders
     

7618f263c4c9d76a47713002160b62f94b126d95
kkmitchell7 <kailen53.mitchell@gmail.com>
2025-01-10 21:32:50 -0800

    Added styles and printing styles

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,17 +1,20 @@
 import React, { useCallback } from 'react'
 import Quill from 'quill'
+import './styles.css'
 import "quill/dist/quill.snow.css"
 
 export default function TextEditor() {
-    const wrapperRef = useCallback((wrapper)=>{
+    //Prevents duplication of quill element upon rerender
+    const wrapperRef = useCallback((wrapper)=>{ //wrapper is div object, useCallback invoked with the DOM element attached/detached from component
         if (wrapper == null) return
-        wrapper.innerHTML = ''
-        const editor = document.createElement('div')
-        wrapper.append(editor)
-        new Quill(editor,{ theme: 'snow'});
+        wrapper.innerHTML = '' //reset the div
+        const editor = document.createElement('div') //create new div
+        wrapper.append(editor) //append new div to wrapper
+        new Quill(editor,{ theme: 'snow'}); //create Quill instance in new div
 
-    },[])
+    },[]) //stable function across renders
+    
     return (
-    <div id="container" ref={wrapperRef}></div>
-  )
+    <div className="container" ref={wrapperRef}></div> //once mounted in the DOM, react passes div to wrapperRef function
+    );
 }

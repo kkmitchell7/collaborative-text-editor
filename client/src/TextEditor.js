b8c87c375ad2ce674189a105e024a841ae5f698c
kkmitchell7 <kailen53.mitchell@gmail.com>
2025-01-11 12:28:03 -0800

    Added event listeners on client and server side to transmit and recieve delta text changes made

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,7 +1,8 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from 'quill'
 import './styles.css'
 import "quill/dist/quill.snow.css"
+import {io} from 'socket.io-client'
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,13 +17,67 @@ const TOOLBAR_OPTIONS = [
   ]
 
 export default function TextEditor() {
-    //Prevents duplication of quill element upon rerender
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+    
+    /**
+     * Opens and cleans up web socket connection
+     */
+    useEffect(()=>{
+        const s = io("http://localhost:3001") //connection
+        setSocket(s)
+
+        return ()=>{ //Cleanup, disconnect web socket when component unmounts
+            s.disconnect()
+        }
+    },[])
+    
+    /**
+     * Recieves text changes from the server
+     */
+    useEffect(()=>{
+        if (socket== null || quill == null) return
+
+        const handler = (delta)=>{ //Delta is quill specific, represents changes made in the editor via small operations
+            quill.updateContents(delta) //When recieve changes from server, use quill to update
+        }
+
+        socket.on('recieve-changes', handler) //Event listener, recieve changes from the server
+
+        return ()=>{ //Cleanup, turn off event listener when component unmounts
+            socket.off('recieve-changes',handler)
+        }
+    },[socket,quill])
+
+    /**
+     * Sends text changes from this instance to the server
+     */
+    useEffect(()=>{
+        if (socket== null || quill == null) return
+
+        const handler = (delta,oldDelta,source)=>{ //Delta is quill specific, represents changes made in the editor via small operations
+            if (source !== 'user') return //Only want to transmit changes made by the user
+            socket.emit('send-changes', delta) //Send the deltas to the server
+        }
+
+        quill.on('text-change', handler) //Event listener, when changes in text, transmit to the socket
+
+        return ()=>{ //Cleanup, turn off event listener when component unmounts
+            quill.off('text-change',handler)
+        }
+    },[socket,quill])
+    
+    
+    /**
+     * Prevents duplication of quill element upon rerender
+     */
     const wrapperRef = useCallback((wrapper)=>{ //wrapper is div object, useCallback invoked with the DOM element attached/detached from component
         if (wrapper == null) return
         wrapper.innerHTML = '' //reset the div
         const editor = document.createElement('div') //create new div
         wrapper.append(editor) //append new div to wrapper
-        new Quill(editor,{ theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}}); //create Quill instance in new div
+        const q = new Quill(editor,{ theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}}); //create Quill instance in new div
+        setQuill(q)
 
     },[]) //stable function across renders
     
---
+++ server/server.js
@@ -0,0 +1,20 @@
+
+const io = require('socket.io')(3001, {
+    cors: { //cors allows support for client & server to have diff urls
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST'],
+    },
+})
+
+
+
+/**
+ * Establishes real time two way communication, persistent connection via the socket
+ * Listens for text changes being sent, and broadcasts these changes to all clients
+ */
+io.on("connection", socket =>{
+    socket.on('send-changes', delta =>{ //Listen for text changes
+        socket.broadcast.emit("recieve-changes",delta) //Broadcast to all connections these changes
+    }) 
+    console.log('connected');
+})
